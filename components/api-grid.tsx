af93cc2f5702422c47495532d44988ea0058983d
tltnn <gm32762499@gmail.com>
2025-07-03 00:09:42 +0200

    Create api-grid.tsx

---
+++ components/api-grid.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { ApiCard } from "./api-card"
+import type { ApiConfig } from "@/types/api"
+
+interface ApiGridProps {
+  apis: ApiConfig[]
+  onTestApi: (api: ApiConfig) => void
+  onShowDocs: (api: ApiConfig) => void
+}
+
+export function ApiGrid({ apis, onTestApi, onShowDocs }: ApiGridProps) {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      {apis.map((api, index) => (
+        <ApiCard
+          key={api.id}
+          api={api}
+          onTest={() => onTestApi(api)}
+          onShowDocs={() => onShowDocs(api)}
+          delay={index * 100}
+        />
+      ))}
+    </div>
+  )
+}
